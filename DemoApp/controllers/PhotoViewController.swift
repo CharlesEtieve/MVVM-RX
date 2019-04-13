83d7278c306c986e80967f894ca256e17d3417e7
Charles Etieve <charles.etieve@gmail.com>
2019-04-13 21:15:47 +0900

    Fix multilingue problems on storyboards

---
+++ DemoApp/controllers/PhotoViewController.swift
@@ -0,0 +1,14 @@
+//
+//  PhotoViewController.swift
+//  DemoApp
+//
+//  Created by Charles Etieve on 13/04/2019.
+//  Copyright © 2019 Charles Etieve. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class PhotoViewController : UIViewController {
+    
+}
